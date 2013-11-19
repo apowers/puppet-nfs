618ca20d6c98445147b99b2e0f0ba9511462820a
Adam Breaux <breauxaj@gmail.com>
2013-11-19 10:44:13 -0500

    revise to only twiddle the nfs service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,11 @@
 define nfs::service (
-  $service = '',
   $ensure = running,
   $enable = true
 ) {
+  $service = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'nfs' ],
+  }
+
   service { $service:
     ensure => $ensure,
     enable => $enable,
