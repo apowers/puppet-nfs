5846a815b911015be10e62fcff56c2bc1440b0b8
Atom Powers <atom.powers@seattlebiomed.org>
2013-12-03 12:49:59 -0800

    fix nasty bad typos

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,16 @@
-define nfs::service (
+# NFS Service
+class nfs::service (
   $ensure = $nfs::service_ensure,
   $enable = $nfs::service_enable
 ) {
   include 'nfs::params'
   $services = $nfs::params::services
+  $packages = $nfs::params::packages
 
   service { $services:
-    ensure => $ensure,
-    enable => $enable,
-    require => Package[$service],
+    ensure  => $ensure,
+    enable  => $enable,
+    require => Package[$packages],
   }
 
 }
